f0b262574f3bcd94b67862561fb6be5be4188f25
Vy Nguyen <vynguyen0520@gmail.com>
2022-08-27 10:44:37 -0700

    removed some console logs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,22 +2,17 @@ import { openDB } from 'idb';
 
 const initdb = async () =>
 // Open a database named 'jate'
-console.log('DIFFERENT ');
   openDB('jate', 1, {
     upgrade(db) {
-      console.log('AFTER DIFFERENT ');
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('SOMETHING ELSE ');
   // create a connection to the db
   const jateDb = await openDB('jate', 1);
 
@@ -41,7 +36,6 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   // create a connection to the db
   const jateDb = await openDB('jate', 1);
-  console.log('574837589430859403895483 ');
   // create a new transaction. make sure it is readonly
   const tx = jateDb.transaction(`jate`, 'readonly');
 
@@ -50,13 +44,10 @@ export const getDb = async () => {
 
   // get all the todos from the store
   const req = store.getAll();
-  console.log('FJSHFIOS JFHIOJHKFO ');
-
   // wait for the request to complete
   const res = await req;
   console.log('🚀 - data retrieved from the database', res);
   return res;
 };
 
-console.log(`something!!!`);
 initdb();
